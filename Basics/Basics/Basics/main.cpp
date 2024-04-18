9a42c5faa939c43d4151f10a50ff9739668cca05
Carpetano <kevincarpetano@proton.me>
2024-04-19 01:40:25 +0200

    Primitives #2 and created arraysAndStrings()

--- Basics/Basics/Basics/main.cpp
+++ Basics/Basics/Basics/main.cpp
@@ -5,11 +5,14 @@
 	below the main method won't be usable without unless declaring like so:
 */
 static void primitivesAndBasics();
+static void arraysAndStrings();
 
 
 int main() {
 
 	primitivesAndBasics();
+
+	arraysAndStrings();
 
 	return 0;
 }
@@ -77,6 +80,18 @@ static void primitivesAndBasics() {
 	std::cin >> myIntInput;
 	std::cout << "You entered: " << myIntInput << std::endl;
 
+
+	/*
+	
+		If statements work just like most programming languages:
+
+		if (condition) {
+			Action performed when true
+		} else {
+			Action performed when false
+		}
+	
+	*/
 	if (myIntInput == 0)
 	{
 		std::cout << "I don't know if it counts as a positive or negative number, I guess it's neither" << std::endl;
@@ -91,8 +106,97 @@ static void primitivesAndBasics() {
 		}
 
 	}
+	/*
+		Switch statements work just like most programming languages 
+		(those that actually have switch statement, I am looking at you python):
+
+		switch(condition)
+		{
+			case <case>: {
+				Action performed when true
+				break;
+			}
+			case <case1>:
+			case <case2>:
+			case <case3>:
+				Action perfomred when either case1 / case2 / case3 was true
+				break;
+			default:
+				Action performed when none of the 'cases' were true
+		}
+	*/
+	switch (myIntInput)
+	{
+		case 1: {
+			std::cout << "Special case: Number is one" << std::endl;
+			break;
+		}
+		case 2: {
+			std::cout << "Special case: Number is two" << std::endl;
+			break;
+		}
+		case 3: {
+			std::cout << "Special case: Number is three" << std::endl;
+			break;
+		}
+		case -1:
+		case -2:
+		case -3: {
+			std::cout << "Special case: -1 / -2 / -3" << std::endl;
+			break;
+		}
+		default: {
+			std::cout << "Not a number considered 'special' ( not 1, 2 or 3 / -3, -2, -1 )" << std::endl;
+			break;
+		}
+	}
+
+
+	/*
+	
+		While loops work just like most programming languages.
+
+		while( condition ) {
+			Action performed as long as condition from above is true
+		}
+
+	*/
+
+	int myLoopInt = -1;
+	
+	while (myLoopInt < 0) {
+		std::cout << "Enter a positive number > ";
+		std::cin >> myLoopInt;
+	}
+
+	/*
+	
+		For loops work just like most programming languages.
+
+		for( <declaration of variable> ; <loop until condition is met> ; <action performed per loop iteration> ) {
+			Action performed until condition from above is true
+		}
+	
+	*/
+
+	std::cout << "Counting from 0 to " << myLoopInt - 1 << std::endl;
+
+	for (int i = 0; i < myLoopInt; i++) {
+		std::cout << " " << i;
+	}
 
 }
+
+
+static void arraysAndStrings() {
+
+	
+
+}
+
+
+
+
 
 
 
